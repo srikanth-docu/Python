7b9c1a7516255fdc678e05a115a5be5cb6b801ca
srikanth-docu <58334531+srikanth-docu@users.noreply.github.com>
2020-05-04 22:38:07 +0530

    Added Python code

---
+++ StringInterpolation.py
@@ -0,0 +1,8 @@
+lang='Python'
+print(f"Learning {lang} Programming")
+
+print("Learning {lang} Programming".format(lang=lang))
+
+print("Learning {} Programming {}".format(lang, '...'))
+
+print("Learning {1} Programming {0}".format('...',lang))
